c252346ea2d828cf7e3249627faffa967ffaf8e3
sepehrgh98 <sepehrghamri@gmail.com>
2020-12-16 00:24:56 +0330

    order page and price page are made!

--- shop/__init__.py
+++ shop/__init__.py
@@ -31,7 +31,7 @@ def create_app(test_config=None):
 
     @app.route("/hello")
     def hello():
-        return "Hello, World!"
+        return render_template('./management/Inventory&price.html')
 
     @app.route("/index")
     def index():
